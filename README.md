aa78ff61e2479eeb3d90873938d02a9be6b431cd
jevilla94 <jvplanesticud@gmail.com>
2018-10-23 09:41:40 -0500

    Inicio de plataforma de administración

---
+++ README.md
@@ -0,0 +1,20 @@
+
+Documentación del proyecto base
+
+[Documentación del proyecto base usando compodoc](https://udistrital.github.io/campus_admin/documentation)
+
+# Plantilla basada en Angular 6+, Bootstrap 4 y <a href="https://github.com/akveo/ngx-admin">Ngx-admin</a>
+
+Proyecto de plataforma CMS-Campus virtual
+Apoyo académico-administrativo de la Universidad
+
+- Proceso de inscripciones
+
+### Demo
+
+<a target="_blank" href="https://pruebascampus.portaloas.udistrital.edu.co/">Demo en línea</a>
+
+## Documentación
+
+[Documentación Udistrital](https://udistrital.github.io/campus_cliente/documentation/).
+[Documentación Ngx-admin](https://akveo.github.io/nebular/docs/installation/based-on-starter-kit-ngxadmin).
---
+++ src/app/pages/pages-menu.ts
@@ -0,0 +1,115 @@
+import { MenuItem } from './menu-item';
+
+export const MENU_ITEMS: MenuItem[] = [
+  {
+    title: 'Dashboard',
+    icon: 'nb-home',
+    link: '/pages/dashboard',
+    home: true,
+    key: 'dashboard',
+  },
+  {
+    title: 'Parametricas',
+    icon: 'nb-compose',
+    link: '/pages/parametricas',
+    key: 'parametricas',
+    children: [
+      {
+        title: 'Genero',
+        link: '/pages/genero/list-genero',
+        key: 'genero',
+      },
+      {
+        title: 'Grupo Etnico',
+        link: '/pages/grupo_etnico/list-grupo_etnico',
+        key: 'grupo_etnico',
+      },
+      {
+        title: 'Estado Civil',
+        link: '/pages/estado_civil/list-estado_civil',
+        key: 'estado_civil',
+      },
+      {
+        title: 'Tipo Discapacidad',
+        link: '/pages/tipo_discapacidad/list-tipo_discapacidad',
+        key: 'tipo_discapacidad',
+      },
+      {
+        title: 'Tipo Lugar',
+        link: '/pages/tipo_lugar/list-tipo_lugar',
+        key: 'tipo_lugar',
+      },
+      {
+        title: 'Tipo Contacto',
+        link: '/pages/tipo_contacto/list-tipo_contacto',
+        key: 'tipo_contacto',
+      },
+      {
+        title: 'Lista Titulacion',
+        link: '/pages/titulacion/list-titulacion',
+        key: 'titulacion',
+      },
+      {
+        title: 'Lista Nivel Formacion',
+        link: '/pages/nivel_formacion/list-nivel_formacion',
+        key: 'nivel_formacion',
+      },
+      {
+        title: 'Lista Metodologia',
+        link: '/pages/metodologia/list-metodologia',
+        key: 'metodologia',
+      },
+      {
+        title: 'Lista Programa Academico',
+        link: '/pages/programa_academico/list-programa_academico',
+        key: 'programa_academico',
+      },
+      {
+        title: 'Lista Enfasis',
+        link: '/pages/enfasis/list-enfasis',
+        key: 'enfasis',
+      },
+      {
+        title: 'Lista Estado Admision',
+        link: '/pages/estado_admision/list-estado_admision',
+        key: 'estado_admision',
+      },
+      {
+        title: 'Lista Linea Investigacion',
+        link: '/pages/linea_investigacion/list-linea_investigacion',
+        key: 'linea_investigacion',
+      },
+    ],
+  },
+  {
+    title: 'Persona',
+    icon: 'nb-compose',
+    link: '/pages/persona',
+    key: 'persona',
+    children: [
+      {
+        title: 'Lista Persona',
+        link: '/pages/persona/list-persona',
+        key: 'lista_persona',
+      },
+      {
+        title: 'CRUD Persona',
+        link: '/pages/persona/crud-persona',
+        key: 'crud_persona',
+      },
+    ],
+  },
+  {
+    title: 'Lugar',
+    icon: 'nb-compose',
+    link: '/pages/lugar',
+    key: 'lugar',
+    children: [
+      {
+        title: 'Lista Lugar',
+        link: '/pages/lugar/list-lugar',
+        key: 'lista_lugar',
+      },
+    ],
+  },
+]
